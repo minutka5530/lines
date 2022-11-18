e6a40284d6262948489c1503c2a14fe458ea4867
reichdarian <minutka5530@gmail.com>
2022-11-18 23:18:25 +0700

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.lang.annotation.Retention;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        String firstName = " Ivanov ";
+        String middleName = " Ivan ";
+        String lastName = " Ivanovich ";
+        String fullName = firstName + middleName +lastName;{
+            System.out.println("ФИО сотрудника - "+ fullName);
+        }
+        // Задача 2
+        String s1 = fullName.toUpperCase();
+        System.out.println("ФИО сотрудника - " + s1);
+        // Задача 3
+        String fullName1 =  "Иванов Семён Семёнович";{
+            fullName1 = fullName1.replace('ё', 'е');
+            System.out.println(fullName1);
+        }
+
+
+
+
     }
+
 }
